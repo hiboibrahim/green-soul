1aed5d957506a2a8a5d855b1f08c69a097340adc
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-04-30 17:27:31 +0000

    hero page

---
+++ green-soul/src/components/About-us/about-us.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './about-us.css'
+
+const about = () => {
+  return (
+    <div>about-us</div>
+  )
+}
+
+export default about-us
--- green-soul/src/components/Hero/Hero.jsx
+++ green-soul/src/components/Hero/Hero.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Hero.css'
 
+
 const Hero = () => {
   return (
-    <div className='hero'> 
+    
+    <div className='hero container'>
+      <div className='hero-text'>
+      <h1>Green Soul - We ignite community led action </h1>
+      <p>We work for and within communities to create transformative experiences and provide opportunity for them to thrive. Our work focuses on building community resilience through holistic, healing practical action.</p>
+      <button className='btn'> Our Projects </button>
+
+
+        </div> 
     
 
-      Hello
     </div>
   )
 }
