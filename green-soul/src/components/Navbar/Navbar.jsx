e67919422ec63ad9ba81c68182dd8cdbd7aa03ed
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-05-01 07:23:57 +0000

    add about component

--- green-soul/src/components/Navbar/Navbar.jsx
+++ green-soul/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import './Navbar.css'
-import logo from '../../assets/greensoul-logo.png'
+import logo from '../../assets/greensoul-logo1.png'
 
 const Navbar = () => {
   return (
     <nav className="container"> 
-        <img src={logo} alt="" className="logo" />
+          <img src={logo} alt="" className="logo" />
         <ul>
             <li>Home</li>
             <li>About Us</li>
