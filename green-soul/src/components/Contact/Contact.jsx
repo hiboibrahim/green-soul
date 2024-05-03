8095ead23ce548ace57999c399b1bd89c5ffb32e
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-05-03 09:03:15 +0000

    create webform

--- green-soul/src/components/Contact/Contact.jsx
+++ green-soul/src/components/Contact/Contact.jsx
@@ -2,18 +2,38 @@ import React from 'react'
 import './Contact.css'
 
 const Contact = () => {
-  return (
+  
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "b8bb0582-ab53-48c5-9717-cd7844fe7920");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
     <div className='contact '>
         <div className='contact-col'>
-            <h3> Curious?  Want to know more?  Want to get involved?  Want help with your project?</h3>
-            <p>Please get in touch! We’d love to hear from you  </p>
-            <ul>
-                <li>greensoul@gmail.com</li>
-                <li>12345677</li>
-
-            </ul>
+            <h3> Want to know more? Want help with your project?</h3>
+            <p>Please get in touch! We’d love to hear from you!  </p>
+           
             <div className='contact-col'>
-                <form>
+                <form onSubmit={onSubmit}>
                     <label>Your Name </label>
                     <input type='text' name='name' placeholder='Enter your name' required/>
                     <label>Email</label>
@@ -24,13 +44,13 @@ const Contact = () => {
                     <textarea name='message' rows='6' placeholder='Enter your message' required></textarea>
                     <button type='submit' className='btn'>Send</button>
                 </form>
-                <span>Sending ...</span>
+                <span>{result}</span>
 
             </div>
 
         </div>
         </div>
-  )
+  
 }
 
 export default Contact
