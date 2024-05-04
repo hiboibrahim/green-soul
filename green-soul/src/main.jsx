5749e311b89fd16fc79487108b777a391a55eda5
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-05-04 20:10:49 +0000

    create router dom

--- green-soul/src/main.jsx
+++ green-soul/src/main.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import aboutus from './Pages/aboutus.jsx'
+import ourprojects from './Pages/ourprojects.jsx'
+import OurProjects from './components/OurProjects/OurProjects.jsx'
+
+const router = createBrowserRouter([{
+  path: '/aboutus', 
+  element: <About />
+},
+{
+  path:'/ourprojects',
+  element:<OurProjects />
+},
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
