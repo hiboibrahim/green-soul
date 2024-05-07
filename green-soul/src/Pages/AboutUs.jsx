b8e2a2173ec08ad7eb974f1b507e08cde4780e2d
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-05-07 18:05:31 +0000

    add about component and team

--- green-soul/src/Pages/AboutUs.jsx
+++ green-soul/src/Pages/AboutUs.jsx
@@ -1,32 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
-import About from '../components/About/About'
+import AboutPage from '../components/AboutPage/AboutPage'
+import Team from '../components/Team/Team'
 
 const AboutUs = () => {
   return (
-    <div className='about-us container'>
-       <h1> About Us </h1>
-       <div className='about-sub'>
-             
-       <h2>Green Soul is a grassroots community organisation centred on social inclusion.</h2>
+    <>
+    <Navbar />
+    <AboutPage />
+    <Team/>
 
-       <h3>We support marginalised groups, those from lower income backgrounds, women and people of colour in engaging in wider society.</h3>
-       
-       
-       
-       <p>Our mission is community led action that creates capacity for transformational shifts. We work for and within communities to create transformative experiences and provide opportunity for them to thrive. Our work focuses on building community resilience through holistic, healing practical action. At Green Soul, we are dedicated to fostering a brighter, more inclusive future for all. 
-
-We firmly believe in the power of community, environmental stewardship, artistic expression, and healing as catalysts for positive change. Our mission is to provide opportunities and create safe spaces for people of colour to engage in forms of personal growth.</p>
-
- 
-
-      
-
-     </div>
-
-    </div>
-    
-    
+    </>
   )
 }
 
---
+++ green-soul/src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+
+const AboutPage = () => {
+  return (
+    <>
+    <Navbar />
+    <div className='about-us container'>
+      
+    </div>
+    
+    <div className='about-section container'>
+
+    <h1 className='about-title'>About Green Soul </h1>
+             
+             <h3>Green Soul is a grassroots community organisation centred on social inclusion.</h3>
+      
+             <h3>We support marginalised groups, those from lower income backgrounds, women and people of colour in engaging in wider society.</h3>
+             
+             
+            
+             <p>Our core values:</p>
+
+             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi earum illum quis voluptas exercitationem eius nemo, quod ipsam, nesciunt fugit at recusandae commodi maiores quaerat. At fugiat hic culpa ex.</p>
+      
+       
+      
+            
+      
+           </div>
+    
+    </>
+  )
+}
+
+export default AboutPage
