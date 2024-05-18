243133c38482b92222f95bd4f9638f138db2c9b1
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-05-18 18:22:55 +0000

    footer links

--- green-soul/src/components/Footer/Footer.jsx
+++ green-soul/src/components/Footer/Footer.jsx
@@ -1,63 +1,41 @@
 import React from 'react'
 import './Footer.css'
-import { FaFacebook, FaInstagram, FaTwitter} from 'react-icons/fa'
 
 
-const sections = [
-  {
-    title: '&copy; 2024 Green Soul All rights reserved.',
-    items:[ 'Privacy Policy', 'Web Design']
-  },
-  {
-    title: 'Green Soul',
-    items:[ 'About Us', 'Our Projects', 'Upcoming events', 'Blog', 'Contact Us', 'Volunteer with Us']
-  },
-
-]
-const items = [
-  {
-    name: 'Facebook',
-    icon: FaFacebook,
-    link: 'https://facebook.com/'
-  },
-  {
-    name: 'Instagram',
-    icon: FaInstagram,
-    link: 'https://instagram.com/'
-  },
-  {
-    name: 'Twitter',
-    icon: FaTwitter,
-    link: 'https://twitter.com/'
-  }
-
-]
 const Footer = () => {
   return (
-    <div className='footer'>
-       
-       <div class="footerContainer">
-        <div class="socialIcons">
-            <a href=""><i class="fa-brands fa-facebook"></i></a>
-            <a href=""><i class="fa-brands fa-instagram"></i></a>
-            <a href=""><i class="fa-brands fa-twitter"></i></a>
-            <a href=""><i class="fa-brands fa-google-plus"></i></a>
-            <a href=""><i class="fa-brands fa-youtube"></i></a>
-        </div>
-        <div class="footerNav">
-            <ul><li><a href="">Home</a></li>
-                <li><a href="">Upcoming Projects</a></li>
-                <li><a href="">About Us</a></li>
-                <li><a href="">Contact Us</a></li>
-                <li><a href="">Volunteer with Us</a></li>
-            </ul>
-        </div>
+    <div class="container">
+    <div class="row">
+      <div class="footer-col">
+        <h4>Green Soul</h4>
+        <ul>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Our Projects</a></li>
+          <li><a href="#">Contact Us</a></li>
+          
+        </ul>
+      </div>
+      <div class="footer-col">
+        <h4>get help</h4>
+        <ul>
+        <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Blog</a></li>
         
+        </ul>
+      </div>
+     
+      <div class="footer-col">
+        <h4>follow us</h4>
+        <div class="social-links">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+      </div>
     </div>
-    <div class="footerBottom">
-        <p>&copy; 2024 Green Soul All rights reserved. <span class="designer">Noman</span></p>
-    </div>
-    </div>
+   </div>
   )
 }
 
