858aef605913b1c4e3bd6024a37c6c9ddab3da59
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-05-24 11:58:19 +0000

    footer

--- green-soul/src/components/Footer/Footer.jsx
+++ green-soul/src/components/Footer/Footer.jsx
@@ -1,94 +1,102 @@
-import React from "react";
-import {
-    Box,
-    FooterContainer,
-    Row,
-    Column,
-    FooterLink
-    
-  
-} from "../Footer/FooterStyles";
- 
-const Footer = () => {
-    return (
-        <Box>
-           
-            <FooterContainer>
-                <Row>
-                    <Column>
-                        <FooterLink href="#">
-                            About Us
-                        </FooterLink>
-                        <FooterLink href="#">
-                            
-                                <span
-                                    style={{
-                                        marginLeft: "10px",
-                                    }}
-                                >
-                                    Instagram
-                                </span>
-                            
-                        </FooterLink>
-                       
-                        
-                    </Column>
-                    <Column>
-                        
-                        <FooterLink href="#">
-                            Our Projects
-                        </FooterLink>
-                        <FooterLink href="#">
-                            
-                                <span
-                                    style={{
-                                        marginLeft: "10px",
-                                    }}
-                                >
-                                    Facebook
-                                </span>
-                            
-                        </FooterLink>
-                       
-                      
-                         
-                    </Column>
-                    <Column>
-                
-                        <FooterLink href="#">
-                            Events
-                        </FooterLink>
-                      
-                        <FooterLink href="#">
-                            
-                                <span
-                                    style={{
-                                        marginLeft: "10px",
-                                    }}
-                                >
-                                    Twitter
-                                </span>
-                            
-                        </FooterLink>
-                      
-                       
-                    </Column>
+import React from 'react';
+import './Footer.css';
+import { Button } from '../Button/Button';
+import { Link } from 'react-router-dom';
 
-                    <Column>
-                        
-                    <FooterLink href="#">
-                            Contact Us 
-                        </FooterLink>
-                       
-                        <FooterLink href="#">
-                            Blog
-                        </FooterLink>
-                      
-                        
-                    </Column>
-                </Row>
-            </FooterContainer>
-        </Box>
-    );
-};
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+Subscribe to stay up to date with our upcoming projects        </p>
+        
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Your Email'
+            />
+            <Button buttonStyle='btn--outline'>Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+           
+            <Link to='/sign-up'>About us</Link>
+            
+          </div>
+          <div class='footer-link-items'>
+            
+            <Link to='/'>Our Projects</Link>
+            
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <Link to='/'>Contact Us</Link>
+           
+          </div>
+          <div class='footer-link-items'>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+         
+          <small class='website-rights'>Green Soul © 2020</small>
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link facebook'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i class='fab fa-instagram' />
+            </Link>
+            <Link
+              class='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i class='fab fa-youtube' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fab fa-linkedin' />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
 export default Footer;
