b568b00afce05e3485b5cbde7d88bbde7f18962e
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-07-20 19:18:50 +0000

    footer

--- green-soul/src/components/Footer/Footer.jsx
+++ green-soul/src/components/Footer/Footer.jsx
@@ -1,62 +1,32 @@
-import React from 'react';
-import './Footer.css';
-import { Button } from '../Button/Button';
-import { Link } from 'react-router-dom';
-import Socials from '../../components/Socials/Socials'
+import React from "react";
+import "./Footer.css";
+import footerimg from "../../assets/footerimg.png";
+import {
+  FiFacebook,
+  FiTwitter,
+  FiInstagram,
+} from "react-icons/fi";
 
-
-
-
-function Footer() {
+const Footer = () => {
   return (
-    <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-Subscribe to stay up to date with our upcoming projects        </p>
-        
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section>
-      <div class='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
-           
-            <Link to='/about'>About us</Link>
-            
-          </div>
-          <div class='footer-link-items'>
-            
-            <Link to='/ourprojects'>Our Projects</Link>
-            
-          </div>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
-            <Link to='/contact'>Contact Us</Link>
-           
-          </div>
-          <div class='footer-link-items'>
-          </div>
-          < Socials />
+    <div className="footer">
+      <div className="footer-container">
+       
+        <div className="all-rights" >
+          <p>© 2024 Green Soul Cardiff. All Rights Reserved
+
+</p>
+<div className="socials">
+        <FiFacebook />
+        <FiTwitter />
+        <FiInstagram />
 
         </div>
-      </div>
-      <div class='social-media-wrap'>
-          <small class='website-rights'>Green Soul © 2024</small>
-          
         </div>
-     
+        
+      </div>
     </div>
   );
-}
+};
 
 export default Footer;
