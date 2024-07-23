76aeda217c25ba34abd0ed03b7a22a2989b710af
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-07-23 12:41:02 +0000

    goals icons

--- green-soul/src/components/Goals/Goals.jsx
+++ green-soul/src/components/Goals/Goals.jsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import './Goals.css'
+import React from "react";
+import "./Goals.css";
 import { FaPaintBrush } from "react-icons/fa";
 import { GiPlantRoots } from "react-icons/gi";
 import { IoFitnessOutline } from "react-icons/io5";
 
-
-
-
 const Goals = () => {
   return (
-    <div className='goals'>
-        <div>
-              <p>Our Goals</p>
-        </div>
-        
-      
+    <div className="goals">
+      <div className="title">
+          <p>Our Goals</p>
 
-            <div>
-                <FaPaintBrush />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quis ab ea dignissimos delectus soluta eaque, veritatis beatae exercitationem omnis ipsam repudiandae repellendus harum impedit aliquid accusantium autem alias illum!</p>
-       
-       <div>
-                <GiPlantRoots />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sed nemo optio enim similique quos. Illo beatae placeat odio, ratione autem quasi officiis officia quo, tempora quisquam enim eaque soluta!</p>
-       </div>
-        <div>
-             <IoFitnessOutline />
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus quia omnis cum placeat non itaque? Deserunt quas unde hic ipsam! Mollitia perspiciatis numquam at cumque? Perferendis tempora consectetur quo laudantium?</p>  
-        </div>
-     
+          
+          </div>
+          <div className="goal-1">
+             <FaPaintBrush size={50}  />
+            <p>
+            Giving access and opportunity to communities of colour in the arts/creative sector.
 
+            </p>
 
+          </div>
+           
+            <div className="goal-2 ">
+              <GiPlantRoots size={50}  />
+              <p>
+              Greening and climate action resources .
+              </p>
             </div>
-        
-    </div>
-  )
-}
+            
+            <div className="goal-3">
+              <IoFitnessOutline size={50}  />
+              <p>
+              Improve mental and physical health and wellbeing
+              </p>
+            </div>
+     
+      </div>
+  );
+};
 
-export default Goals
+export default Goals;
