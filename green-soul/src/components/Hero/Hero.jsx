55f6f7d49ad02381ae2a11dd44519c65e899aae2
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-07-23 15:51:30 +0000

    hero caption

--- green-soul/src/components/Hero/Hero.jsx
+++ green-soul/src/components/Hero/Hero.jsx
@@ -7,8 +7,10 @@ const Hero = () => {
     
     <div className='hero container'>
       <div className='hero-text'>
-      <h1>Green Soul - We ignite community led action </h1>
-      <p>We work for and within communities to create transformative experiences and provide opportunity for them to thrive. Our work focuses on building community resilience through holistic, healing practical action.</p>
+      <h1>We ignite community led action </h1>
+      <p>
+*Advocating for the rights of people and shedding light upon the need for just and equitable systems, access to services and the promotion of community centred health
+</p>
       <button className='btn'> Our Projects </button>
 
 
