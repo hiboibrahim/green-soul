7645b10c825a06e3955c57561ab175084c2ac763
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-08-15 19:53:07 +0000

    footer waves

--- green-soul/src/components/Footer/Footer.jsx
+++ green-soul/src/components/Footer/Footer.jsx
@@ -1,44 +1,47 @@
 import React from "react";
 import "./Footer.css";
-import {
-  FiFacebook,
-  FiTwitter,
-  FiInstagram,
-} from "react-icons/fi";
-import { FaSlack } from "react-icons/fa"
-
-
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { FaSlack } from "react-icons/fa";
 
 const Footer = () => {
   return (
-   
-    
-<footer class="footer" >
-	<div class="waves">
-		<div class="wave" id="wave1"></div>
-		<div class="wave" id="wave2"></div>
-		<div class="wave" id="wave3"></div>
-		<div class="wave" id="wave4"></div>
-	</div>
+    <div className='content-wrapper'>
+      {" "}
+      
+      <footer class="footer">
+        <div class="waves">
+          <div class="wave" id="wave1"></div>
+          <div class="wave" id="wave2"></div>
+          <div class="wave" id="wave3"></div>
+          <div class="wave" id="wave4"></div>
+        </div>
 
-	<ul class="menu">
-		<li class="menu__item"><a class="menu__link" href="#">Home</a></li>
-		<li class="menu__item"><a class="menu__link" href="#">About us</a></li>
-		<li class="menu__item"><a class="menu__link" href="#">Our Projects</a></li>
-    <li class="menu__item"><a class="menu__link" href="#">Connect With Us</a></li>
-
-
-
-	</ul>
-	<p className="copyright-text">Made with 🤍 Hiba Ibrahim</p>
-
-</footer>
-
-           
-
-        
+        <ul class="menu">
+          <li class="menu__item">
+            <a class="menu__link" href="#">
+              Home
+            </a>
+          </li>
+          <li class="menu__item">
+            <a class="menu__link" href="#">
+              About us
+            </a>
+          </li>
+          <li class="menu__item">
+            <a class="menu__link" href="#">
+              Our Projects
+            </a>
+          </li>
+          <li class="menu__item">
+            <a class="menu__link" href="#">
+              Connect With Us
+            </a>
+          </li>
+        </ul>
+        <p className="copyright-text">Made with 🤍 Hiba Ibrahim</p>
+      </footer>
+    </div>
   );
 };
 
 export default Footer;
-
