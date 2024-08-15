89896f651c63ea5c0315369a74f67de46e528dfa
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-08-15 15:22:18 +0000

    navbar

--- green-soul/src/components/Navbar/Navbar.jsx
+++ green-soul/src/components/Navbar/Navbar.jsx
@@ -4,44 +4,48 @@ import menu_icon from "../../assets/menu-icon1.png";
 import { Link } from "react-router-dom";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 import {FaBars, FaSlack } from 'react-icons/fa'
+import logo from '../../assets/Logo1.png'
 
-const Navbar = () => {
+const Navbar = props => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
 
-  const [isOpen,setIsOpen] = useState(false);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  }
+
+  
 
   return (
 <>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav  ms-auto p-2">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/about">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/ourprojects">Our Projects</a>
-      </li>
-     
-    </ul>
-   <ul className="s">
-      <FiFacebook />
-      <FiInstagram />
-      <FiTwitter />
-      <FaSlack />
-    </ul>
-  
-  </div>
-</nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+<div class="container-fluid">
+
+      <a class="navbar-brand text-info font-weight-bolder" href="/">
+        <img src={logo} alt="Logo" width="36" height="36" className="vertical-align-middle" />
+      </a>
+      <button class="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`}>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ABOUT US</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">OUR PROJECTS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold" href="#">CONNECT WITH US</a>
+        </li>
+        
+      </ul>
+
+      </div>
+      </div>
+    </nav>
 
    
     </>
